41fbd68c4ec1026f35fbc11b7d4b5fe3c4d440aa
cryptoforge <cryptoforge.cc@gmail.com>
2020-02-11 20:03:56 -0800

    Clean up

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1205,7 +1205,7 @@ int CWallet::SaplingWitnessMinimumHeight(const uint256& nullifier, int nWitnessH
 
 int CWallet::VerifyAndSetInitialWitness(const CBlockIndex* pindex, bool witnessOnly)
 {
-  LOCK2(cs_wallet,cs_main);
+  LOCK2(cs_main, cs_wallet);
 
   int nWitnessTxIncrement = 0;
   int nWitnessTotalTxCount = mapWallet.size();
@@ -1406,7 +1406,7 @@ int CWallet::VerifyAndSetInitialWitness(const CBlockIndex* pindex, bool witnessO
 void CWallet::BuildWitnessCache(const CBlockIndex* pindex, bool witnessOnly)
 {
 
-  LOCK2(cs_wallet,cs_main);
+  LOCK2(cs_main, cs_wallet);
 
   int startHeight = VerifyAndSetInitialWitness(pindex, witnessOnly) + 1;
 
@@ -3196,7 +3196,7 @@ void CWallet::WitnessNoteCommitment(std::vector<uint256> commitments,
  */
 
 void CWallet::ReorderWalletTransactions(std::map<std::pair<int,int>, CWalletTx> &mapSorted, int64_t &maxOrderPos) {
-    LOCK2(cs_wallet,cs_main);
+    LOCK2(cs_main, cs_wallet);
 
     int maxSortNumber = chainActive.Tip()->GetHeight() + 1;
 
@@ -3222,7 +3222,7 @@ void CWallet::ReorderWalletTransactions(std::map<std::pair<int,int>, CWalletTx> 
  */
 
 void CWallet::UpdateWalletTransactionOrder(std::map<std::pair<int,int>, CWalletTx> &mapSorted, bool resetOrder) {
-  LOCK2(cs_wallet,cs_main);
+  LOCK2(cs_main, cs_wallet);
 
   int64_t previousPosition = 0;
   std::map<const uint256, CWalletTx> mapUpdatedTxs;
@@ -3279,7 +3279,7 @@ void CWallet::DeleteTransactions(std::vector<uint256> &removeTxs) {
 
 void CWallet::DeleteWalletTransactions(const CBlockIndex* pindex) {
 
-      LOCK2(cs_wallet,cs_main);
+      LOCK2(cs_main, cs_wallet);
 
       int nDeleteAfter = (int)fDeleteTransactionsAfterNBlocks;
       bool runCompact = false;
