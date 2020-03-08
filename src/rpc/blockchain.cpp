36c4093d3cd78bc9dd6d1f65dfc8400c0ae4eb46
sol <testnet@vmi273645.contaboserver.net>
2020-03-08 19:10:54 +0100

    Revert "fix for safeids requires escaped quotes for third parameter (parentkey)"
    This reverts commit 407adc87ab6eeff4cb5c53302cc921c8c5c31171.

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1083,7 +1083,7 @@ UniValue safeids(const UniValue& params, bool fHelp, const CPubKey& mypk)
     
     int32_t height;
     if (params.size() >= 1)
-        height = params[0].get_int();
+		height = atoi(params[0].get_str().c_str());
 	else
 		height = chainActive.Height();
 		
